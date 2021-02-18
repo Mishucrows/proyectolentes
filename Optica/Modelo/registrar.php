98ffe4f5043dcd7bf88d24157fe2b23bb5c6d2da
Dennys Rojas <dennys.rojas@espoch.edu.ec>
2021-02-18 15:50:46 -0500

    intento 1 agregar usuarios
    Intento 1 de agregar usuario a la bdd y mostrarlos

---
+++ Optica/Modelo/registrar.php
@@ -0,0 +1,21 @@
+<?php
+
+include("conexionprueba.php");
+
+$nombre=$_POST['validationServer01'];
+$apellido=$_POST['validationServer02'];
+$email=$_POST['validationServer03'];
+$contrasena=$_POST['validationServer04'];
+$nombreUsuario=$_POST['validationServerUsername'];
+$tipoUsuario=$_POST['validationServer05'];
+
+$query="INSERT INTO usuario(nombreUsuario, nombre, apellido, tipoUsuario, correoElectronico, contrasena) VALUES('$nombreUsuario', '$nombre', '$apellido','$tipoUsuario', '$email', '$contrasena')";
+$resultado=$conexion->query($query);
+
+if ($resultado){
+    header("Location: tabla.php");
+}else{
+    echo "No se ha podido guardar el registro";
+}
+
+?>
