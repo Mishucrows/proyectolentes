0fc28776b436d93eddf46e033d466943a30d4ac1
Melthud <49351054+Melthud@users.noreply.github.com>
2021-02-18 23:24:52 -0500

    ingreso productos

---
+++ Optica/Controlador/productos.php
@@ -0,0 +1,14 @@
+<?php 
+// todas las rutas deben establecese desde el index
+include("./Modelo/productos.modelo.php");
+
+class Productos extends Controlador
+{
+    function __constructor(){
+        parent::
+    }
+    function index(){
+        //cargar vista
+    }
+
+}
--- Optica/Controlador/usuario.php
+++ Optica/Controlador/usuario.php
@@ -47,5 +47,6 @@ class Usuario extends Controlador{
         }
 
     }
+
 }
 ?>
