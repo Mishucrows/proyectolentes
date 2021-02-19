fed07d0c1c829d43293bb1486ea3e3823dd43eac
Mishucrows <75451358+Mishucrows@users.noreply.github.com>
2021-02-19 07:14:13 -0500

    Update productos.modelo.php

--- Optica/Modelo/productos.modelo.php
+++ Optica/Modelo/productos.modelo.php
@@ -19,5 +19,38 @@ class ProductoModelo{
         $resultado= $conexion->conectar()->query($query);
         return $resultado;
     }
+    
+    // función modificar
+    function modificar( $id, $nombre, $marca, $tipo, $precio, $material, $sexo){
+        // intanciamos la conexion
+        $conexion = new Cconexion();
+        if (isset($_GET["id"])) //adquirimos el parametro del id
+        {
+            $id = $_GET["id"];
+            $query = "SELECT * FROM producto WHERE id=$id";
+            $result = mysqli_query($con, $query);
+            if (mysqli_num_rows($result) == 1) {
+                $row = mysqli_fetch_array($result); //Estamos ubicandonos en la tabla del id
+                $nombre = $row['nombre'];
+                $marca = $row['marca'];
+                $tipo = $row['tipo'];
+                $precio = $row['precio'];
+                $material = $row['material'];
+                $sexo = $row['sexo'];
+            }
+        }
+        if (isset($_POST['update'])) {
+            $id = $_GET['id'];
+            $nombre= $_POST['nombre']; //estamos modificando los datos
+            $marca = $_POST['marca']; 
+            $tipo = $_POST['tipo'];
+            $precio = $_POST['precio'];
+            $material = $_POST['material'];
+            $sexo = $_POST['sexo'];
+            
+            $query = "UPDATE producto set nombre = '$nombre', marca = '$marca', tipo = '$tipo', precio = '$precio', material = '$material', sexo = '$sexo' WHERE id=$id";
+            mysqli_query($con, $query);
+            echo "Se actualizo correctamente";
+        }
 
 }
