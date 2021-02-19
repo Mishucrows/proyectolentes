ff16d10a4ff0d34150b2b2de846d93f9102744e8
Angello Ordonez <angelo-mjz7@hotmail.com>
2021-02-18 23:04:25 -0500

    implementacion metodo index
    para cargar vistas por defecto

--- Optica/Controlador/inicio.php
+++ Optica/Controlador/inicio.php
@@ -4,6 +4,11 @@ class Inicio extends Controlador{
     function __construct()
     {
         parent::__construct();
+    }
+    /*
+    * Metodo index permite cargar la vista principal por default
+    */
+    function index(){
         parent::cargarvista("html/cliente");
     }
 }
